965a442c1018fc5e4fe048f0cd191225d1e7b6fe
andrejkukucka <andrej.kuku@gmail.com>
2013-04-10 19:33:11 +0200

    headers added

--- src/KapitchiBase/Service/AbstractService.php
+++ src/KapitchiBase/Service/AbstractService.php
@@ -1,8 +1,11 @@
 <?php
 /**
+ * Kapitchi Zend Framework 2 Modules (http://kapitchi.com/)
  *
- * @author Matus Zeman <mz@kapitchi.com>
+ * @copyright Copyright (c) 2012-2013 Kapitchi Open Source Team (http://kapitchi.com/open-source-team)
+ * @license   http://opensource.org/licenses/LGPL-3.0 LGPL 3.0
  */
+
 namespace KapitchiBase\Service;
 
 use Zend\ServiceManager\ServiceLocatorAwareInterface,
